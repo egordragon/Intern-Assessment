70d3931afce5de02b97297cd97f5b944491dcf8b
egordragon <boykoegortr82@gmail.com>
2024-09-12 19:37:36 +0300

    Added set port

--- Task 1/app/app.py
+++ Task 1/app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from routes.routes import init_routes
 from dotenv import load_dotenv
+import os
 
 app = Flask(__name__,template_folder='templates')  #Initialize the Flask app and set the folder for templates
 load_dotenv()
 init_routes(app) # Initialize the app's routes
+
+app.config['PORT']=os.environ.get('PORT')
 
 if __name__ == '__main__':
     app.run() # Start the Flask app
