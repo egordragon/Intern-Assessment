b75d7a70f1475485f02301f959cf4ef48894b8be
egordragon <boykoegortr82@gmail.com>
2024-09-12 20:08:33 +0300

    Enabled debug mode

--- Task 1/app/app.py
+++ Task 1/app/app.py
@@ -8,6 +8,7 @@ load_dotenv()
 init_routes(app) # Initialize the app's routes
 
 app.config['PORT']=os.environ.get('PORT')
+app.config['DEBUG'] = True
 
 if __name__ == '__main__':
     app.run() # Start the Flask app
