ebdeabccf290c73430211e021b59281549b83d34
egordragon <boykoegortr82@gmail.com>
2024-09-12 19:34:58 +0300

    Updated Dockerfile to use gunicorn,

--- Task 1/app/app.py
+++ Task 1/app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes.routes import init_routes
+from dotenv import load_dotenv
 
 app = Flask(__name__,template_folder='templates')  #Initialize the Flask app and set the folder for templates
+load_dotenv()
 init_routes(app) # Initialize the app's routes
 
 if __name__ == '__main__':
-    port = 8080
-    app.run(host='0.0.0.0', port=port ) # Start the Flask app
+    app.run() # Start the Flask app
