d5504cc83c0c791ff1bb30ac7bad367e8c19cc42
egordragon <boykoegortr82@gmail.com>
2024-09-15 12:25:27 +0300

    Changed db URI

--- Task 1/app/config/config.py
+++ Task 1/app/config/config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://mongo:27017/todo_db')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://Egor:todoapp@cluster0.icjoqn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 class TestConfig(Config):
     MONGO_URI = 'mongodb://mongo:27017/todoapptest'
