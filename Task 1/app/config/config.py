b6f959d17943a12bcaea9da96e7a17f4808b95a3
egordragon <boykoegortr82@gmail.com>
2024-09-15 13:42:06 +0300

    Config fix

--- Task 1/app/config/config.py
+++ Task 1/app/config/config.py
@@ -3,5 +3,5 @@ class Config:
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://Egor:todoapp@cluster0.icjoqn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 class TestConfig(Config):
-    MONGO_URI = 'mongodb://mongo:27017/todoapptest'
+    MONGO_URI = 'mongodb+srv://Egor:todoapp@cluster0.icjoqn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     TESTING = True
